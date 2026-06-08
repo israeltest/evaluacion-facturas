8d45b8faba96cbce3f63f438f8416d2eac6a3b41
isra <israel@>
2026-06-07 19:49:12 -0500

    Corrección de import servicio Auth.

--- GestionFactura-Frontend/src/environments/environment.ts
+++ GestionFactura-Frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // URL DE PRODUCCION:
-  // apiUrl: 'https://israelcevme-001-site1.qtempurl.com/api'
+  apiUrl: 'https://israelcevme-001-site1.qtempurl.com/api'
 
   // URL LOCAL PARA PRUEBAS:
-  apiUrl: 'https://localhost:7055/api'
+  // apiUrl: 'https://localhost:7055/api'
 };
